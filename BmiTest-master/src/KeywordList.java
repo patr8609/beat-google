577e05758348db4d9d97060c9f84bf0587572185
WANG YI HSUAN <105304014@mail2.nccu.tw>
2019-01-16 21:46:13 +0800

    first commit

--- BmiTest-master/src/KeywordList.java
+++ BmiTest-master/src/KeywordList.java
@@ -13,24 +13,16 @@ public class KeywordList {
 		keywords.add(new Keyword("feedback",1));
 		keywords.add(new Keyword("good",1));
 		keywords.add(new Keyword("bad",1));		
-		keywords.add(new Keyword("trailers",0.5));
+		keywords.add(new Keyword("trailers",1));
 		keywords.add(new Keyword("similar",0.5));
 		keywords.add(new Keyword("imdb",0.5));
-		keywords.add(new Keyword("rotten tomato",0.5));
+		keywords.add(new Keyword("rotten tomato",100));
 		keywords.add(new Keyword("2018",0.5));
 		keywords.add(new Keyword("2019",1));
 		keywords.add(new Keyword("ptt",0.5));
 //		keywords.add(new Keyword("relieves",0.5));
 		keywords.add(new Keyword("relieving",0.5));
-		keywords.add(new Keyword("Dr",2));
-		keywords.add(new Keyword("doctor",2));
-		keywords.add(new Keyword("health",2));
-		keywords.add(new Keyword("report",1.5));
-		keywords.add(new Keyword("study",1.5));
-		keywords.add(new Keyword("research",1.5));
-		keywords.add(new Keyword("medicine",1.5));
-		keywords.add(new Keyword("treatment",1.5));
-		keywords.add(new Keyword("medical",1.5));
+		
 		
 		
 	}
